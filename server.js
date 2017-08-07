33dc806c439cd2bee242ac880f5ef0a87a8f8584
TrishlaKhabya <30753836+TrishlaKhabya@users.noreply.github.com>
2017-08-07 20:13:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 app.get("/article-one", function (req,res)
 {
-    res.sendFile(path.join(__dirname, 'imad-app', 'article-one.html'));
+    res.sendFile(path.join(__dirname, '', 'article-one.html'));
 });
 
 
