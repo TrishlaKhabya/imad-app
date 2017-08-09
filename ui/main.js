4aa7e1dbd73f6242068af44dbdbae2b98b75b633
TrishlaKhabya <30753836+TrishlaKhabya@users.noreply.github.com>
2017-08-09 19:41:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,18 @@ var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function()
 {
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+    var request=new XMLHttpRequest();
+    request.onreadystatechange = fuction()
+    {
+        if(request.readyState==XMLHttpRequest.DONE)
+            if(request.status==200)
+            {
+                counter=counter+1;
+             var span=document.getElementById('count');
+             span.innerHTML=counter.toString();
+            }
+    };
+    
+    request.open('GET',);
+    
 };
